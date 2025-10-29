b707356719d5189cc06278c15c706d27949267d5
flogit2161 <flo12gab@gmail.com>
2025-10-29 17:25:26 +0100

    first website generation / generate_page function

--- src/functions/generate_page.py
+++ src/functions/generate_page.py
@@ -1,2 +1,21 @@
+from functions.markdown_to_html_node import markdown_to_html_node
+from functions.extract_title import extract_title
+import os
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        src_path_content = f.read()
+    with open(template_path, "r") as f1:
+        template_content = f1.read()
+    from_path_to_html = markdown_to_html_node(src_path_content)
+    content = from_path_to_html.to_html()
+    title = extract_title(src_path_content)
+    final_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    dirpath = os.path.dirname(dest_path)
+    if dirpath and not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    with open(dest_path, "w") as f3:
+        f3.write(final_html)
+
+    
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from functions.assets_to_public import assets_to_public
+from functions.generate_page import generate_page
 
 def main():
     assets_to_public()
+    generate_page("content/index.md", "template.html", "public/index.html")
     
 
 if __name__ == "__main__":
